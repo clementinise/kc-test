4632eccbd6064e7f0db0f42cec1257303c21ca9e
Clément Chevallier <clementinise@gmail.com>
2022-01-06 14:08:49 +0100

    Initial release

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Clementinise'
+description 'KC Test - Used to test HTTP Request, etc'
+version '1.1'
+
+server_script 'main.lua'
+
+kc_updater 'yes'
+kc_name '^2Test Script'
+kc_github 'https://github.com/clementinise/kc-test'
